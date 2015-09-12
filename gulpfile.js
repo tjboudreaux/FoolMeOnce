f1f859d73cd318d368f29b27307829249788a196
Travis Boudreaux <tjboudreaux@gmail.com>
2015-09-11 23:23:57 -0500

    Update package name

--- gulpfile.js
+++ gulpfile.js
@@ -10,67 +10,25 @@ var gulp = require('gulp'),
     zip = require('gulp-zip');
 
 
-
-gulp.task('clean', function() {
-    return gulp.src('build/*', {read: false})
-        .pipe(clean());
-});
-
-//copy static folders to build directory
-gulp.task('copy', function() {
-    gulp.src('src/bg/**')
-        .pipe(gulp.dest('build/bg'));
-    gulp.src('src/options/**')
-        .pipe(gulp.dest('build/options'));
-    gulp.src('src/icons/**')
-        .pipe(gulp.dest('build/icons'));
-    gulp.src('src/_locales/**')
-        .pipe(gulp.dest('build/_locales'));
-    return gulp.src('src/manifest.json')
-        .pipe(gulp.dest('build'));
-});
-
-//copy and compress HTML files
-gulp.task('html', function() {
-    return gulp.src('src/*.html')
-        .pipe(cleanhtml())
-        .pipe(gulp.dest('build'));
-});
-
 //run scripts through JSHint
 gulp.task('jshint', function() {
-    return gulp.src('src/scripts/*.js')
+    return gulp.src('src/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
-//copy vendor scripts and uglify all other scripts, creating source maps
-gulp.task('scripts', ['jshint'], function() {
-    gulp.src('src/scripts/vendors/**/*.js')
-        .pipe(gulp.dest('build/scripts/vendors'));
-    return gulp.src(['src/scripts/**/*.js', '!src/scripts/vendors/**/*.js'])
-        .pipe(stripdebug())
-        .pipe(uglify({outSourceMap: true}))
-        .pipe(gulp.dest('build/scripts'));
-});
-
-
 //build ditributable and sourcemaps after other tasks completed
-gulp.task('zip', ['html', 'scripts', 'copy'], function() {
+gulp.task('zip', ['jshint'], function() {
     var manifest = require('./src/manifest'),
-        distFileName = manifest.short_name + ' v' + manifest.version + '.zip',
-        mapFileName = manifest.short_name + ' v' + manifest.version + '-maps.zip';
-    //collect all source maps
-    gulp.src('build/scripts/**/*.map')
-        .pipe(zip(mapFileName))
-        .pipe(gulp.dest('dist'));
+        distFileName = manifest.short_name + ' v' + manifest.version + '.zip';
+
     //build distributable extension
-    return gulp.src(['build/**', '!build/scripts/**/*.map'])
+    return gulp.src(['src/**'], {cwd:'src'})
         .pipe(zip(distFileName))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest(__dirname + '/dist'));
 });
 
 //run all tasks after build directory has been cleaned
-gulp.task('default', ['clean'], function() {
+gulp.task('default', [], function() {
     gulp.start('zip');
 });
